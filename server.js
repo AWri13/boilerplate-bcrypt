0678e0d96ea2193bdd9e8f0950b2b5d6286626d0
Amanda Wright <148620370+AWri13@users.noreply.github.com>
2024-04-10 20:02:08 -0400

    async

--- server.js
+++ server.js
@@ -11,13 +11,14 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-bcrypt.hash('sUperpassw0rd!', 12, (err, hash) => {
+const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
     console.log(hash);
-    //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
-    bcrypt.compare('pass123', hash, (err, res) => {
-      console.log(res); //true
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res);
     });
-  });
+
+});
+
 
 
 //END_ASYNC
